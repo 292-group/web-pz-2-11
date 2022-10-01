aced6177d1f7af9b6f62f4db65ecd5d841b0caae
Andy Mychka <I.Muchka73@gmail.com>
2022-10-01 19:52:08 +0300

    (fix)Reset before all css style,got all list of characters from API,added infinity scroll,renamed a func to HttReque and changed cucle for create list with characters using jquery !!!IN ONE LINE and WITHOUT createElement!!!

--- js/main.js
+++ js/main.js
@@ -1,41 +1,35 @@
 let number;
-let block;
-for(let i=1;i<=15;i++){
-    block=document.createElement('li');
-    block.innerHTML=(`Character ${i}`);
-    block.className=(`${i}`);
-    document.querySelector('.character-list').appendChild(block);
-}
+// cucle for create the list with characters
+for(let i=1;i<=2138;i++) $('.character-list').append(`<li class='${i}'>Character ${i}</li>`);
+//click on list with character
 document.querySelector('.character-list ').onclick=(event)=>{
     number=parseInt((event.target.className));
-    number=`${number+50}`;
-  
     const xhttp= new XMLHttpRequest();
-
+//api
 xhttp.onreadystatechange=function(){
     if (this.readyState==4&&this.status==200){
-        myFunction(JSON.parse(this.responseText) )
+        HttRequest(JSON.parse(this.responseText))
     }
 }
 xhttp.open('GET',`https://anapioficeandfire.com/api/characters/${number}`);
 xhttp.setRequestHeader("Content-Type","text/plain;charset=UTF-8");
 xhttp.send();
-
-function myFunction(data){
-   document.querySelector('.name').innerHTML=`${data.name}`
-   document.querySelector('.info_character').innerHTML=`
-                <li class="gender"><span>Gender: </span>${data.gender}</li>
-                <li class="gender"><span>Culture: </span>${data.culture}</li>
-                <li class="gender"><span>Born: </span>${data.born}</li>
-                <li class="gender"><span>Died: </span>${data.died}</li>
-                <li class="gender"><span>Title: </span>${data.titles}</li>
-                <li class="gender"><span>Aliasees: </span>${data.aliases}</li>
-                <li class="gender"><span>Father: </span>${data.father}</li>
-                <li class="gender"><span>Mather: </span>${data.mother}</li>
-                <li class="gender"><span>Spouse: </span>${data.spouse}</li>
-                <li class="gender"><span>Books: </span>${data.books}</li>
-                <li class="gender"><span>PovBooks: </span>${data.povBooks}</li>
-                <li class="gender"><span>Played By: </span>${data.playedBy}</li>            
-   `
-}
+//func for output info about character
+function HttRequest(data){
+    document.querySelector('.name').innerHTML=`${data.name}`
+    document.querySelector('.info_character').innerHTML=`
+                 <li><span>Gender: </span>${data.gender}</li>
+                 <li><span>Culture: </span>${data.culture}</li>
+                 <li><span>Born: </span>${data.born}</li>
+                 <li><span>Died: </span>${data.died}</li>
+                 <li><span>Title: </span>${data.titles}</li>
+                 <li><span>Aliasees: </span>${data.aliases}</li>
+                 <li><span>Father: </span>${data.father}</li>
+                 <li><span>Mather: </span>${data.mother}</li>
+                 <li><span>Spouse: </span>${data.spouse}</li>
+                 <li><span>Books: </span>${data.books}</li>
+                 <li><span>PovBooks: </span>${data.povBooks}</li>
+                 <li><span>Played By: </span>${data.playedBy}</li>            
+    `
+ }
 }
